02a50b54e6f1cbe2de6455292638cdd37379ccab
Roger Demetrescu <roger.demetrescu@gmail.com>
2020-09-23 11:21:40 -0300

    add postgresql to the project
    + updated docker configuration
    + config module that reads a .env file
    + fastapi connects and closes connections properly

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.core import config, tasks
+
 
 def get_application():
-    app = FastAPI(title="Online Store", version="0.0.1")
+    app = FastAPI(title=config.PROJECT_NAME, version=config.VERSION)
     app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
@@ -11,6 +13,10 @@ def get_application():
         allow_methods=["*"],
         allow_headers=["*"],
     )
+
+    app.add_event_handler("startup", tasks.create_start_app_handler(app))
+    app.add_event_handler("shutdown", tasks.create_stop_app_handler(app))
+
     return app
 
 
